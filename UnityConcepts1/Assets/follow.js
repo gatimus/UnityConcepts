009f4faf3c1bad8bd74a17b1743152bf856756ee
gatimus <william.g.lohan@gmail.com>
2015-01-09 23:31:34 -0800

    home 1/9

---
+++ UnityConcepts1/Assets/follow.js
@@ -0,0 +1,22 @@
+﻿var target : Transform; 
+var moveSpeed = 6; 
+var rotationSpeed = 6; 
+var myTransform : Transform; 
+
+function Awake() {
+	myTransform = transform; 
+}
+
+function Start() {
+	target = GameObject.FindWithTag("Player").transform; 
+}
+
+function Update () {
+   if (!renderer.isVisible){
+    myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+    Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime); 
+    myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+    }
+}
+
+
---
+++ UnityConcepts1/Assets/kill.js
@@ -0,0 +1,11 @@
+﻿function OnTriggerEnter (other : Collider) {
+	print("trigger");
+	if (other.tag == "Player"){
+		print("with player");
+		Application.LoadLevel(0);
+	}
+}
+
+function OnCollisionEnter(collision : Collision) {
+	print("collision");
+}
